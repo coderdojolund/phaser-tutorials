510e4e3581843f69939ba17d6fafad06bd53b5a5
Robert Storlind <carlrobert@users.noreply.github.com>
2016-06-27 17:03:20 +0200

    First version with js/html split
    jslint compliant

---
+++ phaser_tutorial_02/part1.js
@@ -0,0 +1,16 @@
+/*global Phaser */
+/*jslint node: true */
+'use strict';
+
+var game;
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
---
+++ phaser_tutorial_02/part2.js
@@ -0,0 +1,22 @@
+/*global Phaser */
+/*jslint node: true */
+'use strict';
+
+var game;
+
+function preload() {
+
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+
+}
+
+function create() {
+}
+
+function update() {
+}
+
+game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
