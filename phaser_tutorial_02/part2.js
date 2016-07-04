ba588093f05d967a8444e5f649ae4c124088c191
Robert Storlind <carlrobert@users.noreply.github.com>
2016-07-04 22:16:13 +0200

    Whitespace compression

--- phaser_tutorial_02/part2.js
+++ phaser_tutorial_02/part2.js
@@ -5,12 +5,10 @@
 var game;
 
 function preload() {
-
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-
 }
 
 function create() {
